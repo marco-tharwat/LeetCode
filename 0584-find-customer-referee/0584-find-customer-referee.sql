b9dca2edd180b4b5eb8fe14f60085a7298947d3f
Marco Tharwat <markotharwat11@gmail.com>
2026-07-13 14:01:45 +0300

    Time: 658 ms (14.66%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,3 +1,4 @@
 /* Write your T-SQL query statement below */
 
-select name from Customer where referee_id is null or referee_id != 2
+select name from Customer
+ where referee_id is null or referee_id != 2
