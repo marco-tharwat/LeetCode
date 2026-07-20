7e84e3c2fd4c830ac17800ed373a935f349d3306
Marco Tharwat <markotharwat11@gmail.com>
2026-07-20 18:31:20 +0300

    Time: 556 ms (23.83%), Space: 0B (100%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -0,0 +1,2 @@
+/* Write your T-SQL query statement below */
+select w1.id from Weather w1 join Weather w2 on w1.recordDate > w2.recordDate where w1.temperature > w2.temperature and DATEDIFF(day, w2.recordDate, w1.recordDate) = 1
