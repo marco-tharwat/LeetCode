b2b4c06556ee42b1ed1f7d255367611f3e0dd41a
Marco Tharwat <markotharwat11@gmail.com>
2026-07-29 22:41:43 +0300

    Time: 458 ms (67.33%), Space: 0B (100%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,2 @@
+/* Write your T-SQL query statement below */
+select s.user_id, round(avg(case when c.action = 'confirmed' then 1.0 else 0.0 end),2) as confirmation_rate  from Signups s left join Confirmations c on s.user_id = c.user_id group by s.user_id
